fee59fd11fccc467a986c176c913a8d16a0014e5
Oleh Denysov <olegonuk@gmail.com>
2020-12-22 15:48:42 +0200

    Day_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,28 @@
+// Импорт функций с других файлов js
+import { videoPlayerInit } from './videoPlayer.js';
 import { radioPlayerInit } from './radioPlayer.js';
 import { musicPlayerInit } from './musicPlayer.js';
-import { videoPlayerInit } from './videoPlayer.js';
 
 
+//Объявление переменных
 const playerBtn = document.querySelectorAll('.player-btn'),
    playerBlock = document.querySelectorAll('.player-block'),
    temp = document.querySelector('.temp');
 
+// Функция удаления класса активности у элементов
 const deactivationPlayer = () => {
    temp.style.display = 'none';
    playerBtn.forEach(item => item.classList.remove('active'));
    playerBlock.forEach(item => item.classList.remove('active'));
 }
 
+//Добавляем класс активности на элементы
 playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
-   deactivationPlayer();
-   btn.classList.add('active');
-   playerBlock[i].classList.add('active');
+   deactivationPlayer();//удаляем классы активности у элементов
+   btn.classList.add('active'); //Добавляем класс "эктив" элементу по которому был клик
+   playerBlock[i].classList.add('active'); //Добавляем класс "эктив" элементу используя индекс элемента по которому был клик
 }));
 
-
+videoPlayerInit();
 radioPlayerInit();
 musicPlayerInit();
-videoPlayerInit();
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,73 @@
 export const videoPlayerInit = () => {
-   console.log('Video Init');
+
+   //Объявление переменных
+   const videoPlayer = document.querySelector('.video-player'),
+      videoButtonPlay = document.querySelector('.video-button__play'),
+      videoButtonStop = document.querySelector('.video-button__stop'),
+      videoTimePassed = document.querySelector('.video-time__passed'),
+      videoProgress = document.querySelector('.video-progress'),
+      videoTimeTotal = document.querySelector('.video-time__total');
+
+   // Функция смены иконки плэй/пауза
+   const toggleIcon = () => {
+      if (videoPlayer.paused) {
+         videoButtonPlay.classList.remove('fa-pause');
+         videoButtonPlay.classList.add('fa-play');
+      } else {
+         videoButtonPlay.classList.add('fa-pause');
+         videoButtonPlay.classList.remove('fa-play');
+      }
+   }
+
+   // Фуекция запуска видео
+   const togglePlay = () => {
+      if (videoPlayer.paused) {
+         videoPlayer.play();
+      } else {
+         videoPlayer.pause();
+      }
+   }
+
+   //Функция останвки видео
+   const stopPlay = () => {
+      videoPlayer.pause();
+      videoPlayer.currentTime = 0; // Сброс время видео
+   }
+
+   // Функция прибавление "ноль" к цыфрам меньше "10"
+   const addZero = n => n < 10 ? '0' + n : n;
+
+   // Навешиваем обработчик события
+   videoPlayer.addEventListener('click', togglePlay);
+   videoButtonPlay.addEventListener('click', togglePlay);
+   videoButtonStop.addEventListener('click', stopPlay);
+
+   // События плэера
+   videoPlayer.addEventListener('play', toggleIcon);
+   videoPlayer.addEventListener('pause', toggleIcon);
+
+   // Счетчики времени
+   videoPlayer.addEventListener('timeupdate', () => {
+      const currentTime = videoPlayer.currentTime; // Текущее время видео
+      const durationTime = videoPlayer.duration; // Общее время видео
+
+      videoProgress.value = (currentTime / durationTime) * 100; //Установка прогресса видео
+
+      let minutePassed = Math.floor(currentTime / 60); //Секунды в минуты
+      let secondPassed = Math.floor(currentTime % 60); //Остаток от минут - секунды
+
+      let minuteTotal = Math.floor(durationTime / 60); //Секунды в минуты
+      let secondTotal = Math.floor(durationTime % 60); //Остаток от минут - секунды
+
+      videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondPassed)}`; //выводим время на странице
+      videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondTotal)}`; //выводим время на странице
+   });
+
+   // Событие изменения прогресса видео "клик в прогресс ленте"
+   videoProgress.addEventListener('change', () => {
+      const durationTime = videoPlayer.duration; // Общее время видео
+      const value = videoProgress.value; // Значение при изменении прогрес видео - клик на прогресс ленте
+
+      videoPlayer.currentTime = (value * durationTime) / 100; //Устанавливаем время видео на то месо где был клик в прогресс ленте
+   });
 }
