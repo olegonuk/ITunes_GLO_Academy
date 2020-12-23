df843a826adc1542a422339c825d8a3cbdf55bb7
Oleh Denysov <olegonuk@gmail.com>
2020-12-23 17:03:50 +0200

    Day_3

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,70 @@
 export const radioPlayerInit = () => {
-   console.log('Radio Init');
+
+   //Объявление переменных
+   const radio = document.querySelector('.radio'),
+      radioCoverImg = document.querySelector('.radio-cover__img'),
+      radioNavigation = document.querySelector('.radio-navigation'),
+      radioHeaderBig = document.querySelector('.radio-header__big'),
+      radioItem = document.querySelectorAll('.radio-item'),
+      radioStop = document.querySelector('.radio-stop');
+
+   //Создание объекта Аудио
+   const audio = new Audio();
+   audio.type = 'audio/aac'; //Формат аудио
+
+   radioStop.disabled = true; //Отключение кнопки плэй
+
+   //Функция смены иконки плэй/пауза
+   const changeIconPlay = () => {
+      if (audio.paused) {
+         radio.classList.remove('play');
+         radioStop.classList.add('fa-play');
+         radioStop.classList.remove('fa-pause');
+      } else {
+         radio.classList.add('play');
+         radioStop.classList.add('fa-pause');
+         radioStop.classList.remove('fa-play');
+      }
+   }
+   //Функция выбора радиостпнции
+   const selectItem = elem => {
+      radioItem.forEach(item => item.classList.remove('select'));
+      elem.classList.add('select');
+   }
+   //Обработчик события на переключение между радиостанциями
+   radioNavigation.addEventListener('change', event => {
+      const target = event.target;
+      const parrent = target.closest('.radio-item');
+      selectItem(parrent);
+
+      //Заголовок радиостанции
+      const title = parrent.querySelector('.radio-name').textContent;
+      radioHeaderBig.textContent = title;
+
+      //Смена картинки радиостанции
+      const urlImg = parrent.querySelector('.radio-img').src;
+      radioCoverImg.src = urlImg;
+
+      radioStop.disabled = false;
+      audio.src = target.dataset.radioStantion; //Адресс радиостанции
+      audio.play();
+      changeIconPlay();
+   });
+
+   radioStop.addEventListener('click', () => {
+      if (audio.paused) {
+         audio.play();
+      } else {
+         audio.pause();
+      }
+      changeIconPlay();
+   });
+
+
+
+
+
+
+
+
 }
